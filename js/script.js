e144830b1120544fa04f752e63f688316ce8fdb5
wlwl2 <manvinedi@gmail.com>
2016-02-28 21:30:42 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-document.querySelectorAll(".menu > ul > li").addEventListener;
+showMenu = function () {
+  if ( this.nextElementSibling.className === "hide-sub-menu") {
+    this.nextElementSibling.className = "show-sub-menu";
+  } else {
+    this.nextElementSibling.className = "hide-sub-menu";
+  }
+}
+
+for (i=0; i<document.querySelectorAll(".menu > ul > li").length; i++) {
+  if (document.querySelectorAll(".menu > ul > li > button")[i].nextElementSibling) {
+    document.querySelectorAll(".menu > ul > li > button")[i].addEventListener("click", showMenu);
+  }
+}
