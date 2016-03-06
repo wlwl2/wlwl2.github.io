04a30d6316ce3b39ab465e06074a76ef11b99265
wlwl2 <manvinedi@gmail.com>
2016-03-06 19:15:21 +0800

    update

--- js/cube.js
+++ js/cube.js
@@ -1,11 +1,11 @@
-//3.js
+//3.js (see script.js for more manipulation of the cube)
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 20, 150/50, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.setSize( 150, 50);
 
-document.body.insertBefore( renderer.domElement, document.querySelector(".menu") );
+document.body.insertBefore( renderer.domElement, document.querySelector(".menu-container") );
 
 var ambientLight = new THREE.AmbientLight( 0x000000 );
     scene.add( ambientLight );
@@ -18,32 +18,8 @@ var ambientLight = new THREE.AmbientLight( 0x000000 );
     scene.add( lights[0] );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshLambertMaterial( { color: 0x090e33 } );
+var material = new THREE.MeshLambertMaterial( { color: 0x54e7a0 } );
 var cube = new THREE.Mesh( geometry, material );
-
-var onKeyDown = function(event) {
-  if (event.keyCode == 67) { // when 'c' is pressed
-    cube.material.color.setHex(0xfc00b6); // there is also setHSV and setRGB
-  }
-  else if (event.keyCode == 68) { // when 'd' is pressed
-    cube.material.color.setHex(0x24ff33); // there is also setHSV and setRGB
-  }
-  else if (event.keyCode == 69) { // when 'd' is pressed
-    cube.material.color.setHex(0x090e33); // there is also setHSV and setRGB
-  }
-};
-document.addEventListener('keydown', onKeyDown, false);
-
-// this handler will be executed every time the cursor is moved over a different list item
-  document.querySelector("canvas").addEventListener("mouseover", function( event ) {
-    // highlight the mouseover target
-    cube.material.color.setHex(0x0955ff); // there is also setHSV and setRGB
-  }, false);
-
-  document.querySelector("canvas").addEventListener("mouseout", function( event ) {
-    // highlight the mouseover target
-    cube.material.color.setHex(0x090e33); // there is also setHSV and setRGB
-  }, false);
 
 scene.add( cube );
 
--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+// Click to show sub menus.
 showMenu = function () {
+  // If the menu is hidden, then show the menu. Color of the cube becomes red.
   if ( this.nextElementSibling.className === "hide-sub-menu") {
     this.nextElementSibling.className = "show-sub-menu";
-  } else {
+    cube.material.color.setHex(0xe32121); // there is also setHSV and setRGB
+  } else { // If the menu is shown, then hide the menu. Color of the cube becomes green.
     this.nextElementSibling.className = "hide-sub-menu";
+    cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
   }
-}
-
-for (i=0; i<document.querySelectorAll(".menu > ul > li").length; i++) {
-  if (document.querySelectorAll(".menu > ul > li > button")[i].nextElementSibling) {
-    document.querySelectorAll(".menu > ul > li > button")[i].addEventListener("click", showMenu);
+};
+var menuButtons = document.querySelectorAll(".menu > ul > li > button");
+for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
+  if (menuButtons[i].nextElementSibling) {
+    menuButtons[i].addEventListener("click", showMenu);
   }
 }
