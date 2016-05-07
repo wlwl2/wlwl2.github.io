fe4b83b2e52acf0c49a85cb7356b2266a8aeb19f
wlwl2 <manvinedi@gmail.com>
2016-05-07 12:09:32 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+// Just copied and pasted lines 5 to 11 need to optimize.
 // Click to show sub menus.
 showMenu = function (event) {
   event.stopPropagation();
+  for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
+    if (menuList[i].firstChild.nextElementSibling) {
+      menuList[i].firstChild.className = "sub-menu-button hide-sub-menu";
+      cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
+    }
+  }
   // If the menu is hidden, then show the menu. Color of the cube becomes red.
   if ( this.nextElementSibling.className === "sub-menu hide-sub-menu") {
     for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
@@ -8,9 +15,11 @@ showMenu = function (event) {
        menuList[i].firstChild.nextElementSibling.className = "sub-menu hide-sub-menu";
       }
     }
+    this.className = "sub-menu-button show-sub-menu";
     this.nextElementSibling.className = "sub-menu show-sub-menu";
     cube.material.color.setHex(0xe32121); // there is also setHSV and setRGB
   } else { // If the menu is shown, then hide the menu. Color of the cube becomes green.
+    this.className = "sub-menu-button hide-sub-menu";
     this.nextElementSibling.className = "sub-menu hide-sub-menu";
     cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
   }
@@ -28,11 +37,13 @@ for (var i=0; i<menuButtons.length; i++) {
   }
 }
 
+// Click outside the menu to close it. This adds an event listener to the whole page.
 document.addEventListener('click', function() {
   for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
     if (menuList[i].firstChild.nextElementSibling) {
-     menuList[i].firstChild.nextElementSibling.className = "sub-menu hide-sub-menu";
-     cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
+      menuList[i].firstChild.className = "sub-menu-button hide-sub-menu";
+      menuList[i].firstChild.nextElementSibling.className = "sub-menu hide-sub-menu";
+      cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
     }
   }
 });
