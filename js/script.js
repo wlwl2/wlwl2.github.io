4af1a3a1fa67376cdc329070ebf601a126145c72
William Lim <manvinedi@gmail.com>
2016-07-10 02:36:52 +0800

    update

--- js/script.js
+++ js/script.js
@@ -3,25 +3,19 @@
 showMenu = function (event) {
   event.stopPropagation();
   for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
-    if (menuList[i].firstChild.nextElementSibling) {
-      menuList[i].firstChild.className = "sub-menu-button hide-sub-menu";
-      cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
-    }
   }
   // If the menu is hidden, then show the menu. Color of the cube becomes red.
-  if ( this.nextElementSibling.className === "sub-menu hide-sub-menu") {
+  if ( this.nextElementSibling.className === "sub-menu hidden") {
     for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
       if (menuList[i].firstChild.nextElementSibling) {
-       menuList[i].firstChild.nextElementSibling.className = "sub-menu hide-sub-menu";
+       menuList[i].firstChild.nextElementSibling.className = "sub-menu hidden";
       }
     }
-    this.className = "sub-menu-button show-sub-menu";
-    this.nextElementSibling.className = "sub-menu show-sub-menu";
-    cube.material.color.setHex(0xe32121); // there is also setHSV and setRGB
-  } else { // If the menu is shown, then hide the menu. Color of the cube becomes green.
-    this.className = "sub-menu-button hide-sub-menu";
-    this.nextElementSibling.className = "sub-menu hide-sub-menu";
-    cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
+    this.className = "menu-button shown";
+    this.nextElementSibling.className = "sub-menu shown";
+  } else {
+    this.className = "menu-button hide-sub-menu";
+    this.nextElementSibling.className = "sub-menu hidden";
   }
 };
 
@@ -29,7 +23,7 @@ stopClose = function (event) {
   event.stopPropagation();
 };
 
-var menuButtons = document.querySelectorAll(".menu > ul > li > button");
+var menuButtons = document.querySelectorAll(".menu > ul > li > a");
 for (var i=0; i<menuButtons.length; i++) {
   if (menuButtons[i].nextElementSibling) {
     menuButtons[i].addEventListener("click", showMenu);
@@ -39,11 +33,10 @@ for (var i=0; i<menuButtons.length; i++) {
 
 // Click outside the menu to close it. This adds an event listener to the whole page.
 document.addEventListener('click', function() {
-  for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
+  for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i < menuList.length; i++) {
     if (menuList[i].firstChild.nextElementSibling) {
-      menuList[i].firstChild.className = "sub-menu-button hide-sub-menu";
-      menuList[i].firstChild.nextElementSibling.className = "sub-menu hide-sub-menu";
-      cube.material.color.setHex(0x54e7a0); // there is also setHSV and setRGB
+      menuList[i].firstChild.className = "menu-button hide-sub-menu";
+      menuList[i].firstChild.nextElementSibling.className = "sub-menu hidden";
     }
   }
 });
