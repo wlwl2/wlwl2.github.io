8981391872c4d3f4325dc2c705cd2c877defdd6e
William Lim <manvinedi@gmail.com>
2016-07-17 22:16:16 +0800

    Revert "update"
    This reverts commit 89002adee4f9bd874cbcef225bede8335593c368.

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        var repoList = document.querySelector(".repository-list");
+        var repoList = document.querySelector(".menu > ul").children[1].children[1];
         var myRepositoriesObject = JSON.parse(httpRequest.responseText);
         for (var i = 0, repoListItems = "";i<myRepositoriesObject.length;i++) {
           var repoListItems = repoListItems +
@@ -29,13 +29,43 @@
 
 // need to optimize.
 // Click to show sub menus.
-showSection = function (event) {
+showMenu = function (event) {
+  event.stopPropagation();
+  for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
+  }
+  // If the menu is hidden, then show the menu. Color of the cube becomes red.
+  if ( this.nextElementSibling.className === "sub-menu hidden") {
+    for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i<menuList.length; i++) {
+      if (menuList[i].firstChild.nextElementSibling) {
+       menuList[i].firstChild.nextElementSibling.className = "sub-menu hidden";
+      }
+    }
+    this.className = "menu-button shown";
+    this.nextElementSibling.className = "sub-menu shown";
+  } else {
+    this.className = "menu-button hide-sub-menu";
+    this.nextElementSibling.className = "sub-menu hidden";
+  }
+};
 
+stopClose = function (event) {
+  event.stopPropagation();
 };
 
 var menuButtons = document.querySelectorAll(".menu > ul > li > a");
 for (var i=0; i<menuButtons.length; i++) {
   if (menuButtons[i].nextElementSibling) {
     menuButtons[i].addEventListener("click", showMenu);
+    menuButtons[i].nextElementSibling.addEventListener("click", stopClose);
   }
 }
+
+// Click outside the menu to close it. This adds an event listener to the whole page.
+document.addEventListener('click', function() {
+  for (var i=0, menuList = document.querySelectorAll(".menu > ul > li"); i < menuList.length; i++) {
+    if (menuList[i].firstChild.nextElementSibling) {
+      menuList[i].firstChild.className = "menu-button hide-sub-menu";
+      menuList[i].firstChild.nextElementSibling.className = "sub-menu hidden";
+    }
+  }
+});
